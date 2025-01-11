ffc9694f241a335216cd9c021297b15c3da9e361
Arun <arunm102301@gmail.com>
2025-01-11 18:49:49 +0530

    banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import banner1 from '../../public/banner1.jpg';
+
+function Banner() {
+
+    function bannerImageChange () {
+        let bannerImages = [banner1, banner0, banner1, banner1];
+        let bannerIndex = 0;
+        setInterval(() => {
+            bannerIndex++;
+            if(bannerIndex >= bannerImages.length){
+                bannerIndex = 0;
+            }
+            document.querySelector('.bg-cover').style.backgroundImage = `url(${bannerImages[bannerIndex]})`;
+        }, 3000);
+    }
+
+    
+  return (
+    <div className='h-[20vh] md:h-[75vh] bg-cover items-end flex bg-center' onChange={bannerImageChange}>
+        <div className="flex bg-black bg-opacity-50 w-[100vw] justify-center">
+            <h1 className='text-2xl font-bold text-white p-2'>Mission Impossible Dead Reckoning</h1>
+        </div> 
+        
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Banner from './Banner';
 
 function Movies() {
   return (
     <div>
+        <Banner />
         <h1>welcome-to</h1>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
