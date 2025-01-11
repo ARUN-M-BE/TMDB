89e495924305db82eda597f1aa239616caee44b2
Arun <arunm102301@gmail.com>
2025-01-11 19:33:19 +0530

    now

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import banner1 from '../../public/banner1.jpg';
 function Banner() {
 
     function bannerImageChange () {
-        let bannerImages = [banner1, banner0, banner1, banner1];
+        let bannerImages = [banner1, banner1, banner1, banner1];
         let bannerIndex = 0;
         setInterval(() => {
             bannerIndex++;
@@ -15,7 +15,7 @@ function Banner() {
         }, 3000);
     }
 
-    
+
   return (
     <div className='h-[20vh] md:h-[75vh] bg-cover items-end flex bg-center' onChange={bannerImageChange}>
         <div className="flex bg-black bg-opacity-50 w-[100vw] justify-center">
