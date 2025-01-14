fc9234ecec4b24dcf7abbfebc6c3b5b704aabf17
Arun <arunm102301@gmail.com>
2025-01-14 14:30:00 +0530

    jjj

--- src/components/Pagenation.jsx
+++ src/components/Pagenation.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Pagenation() {
 
@@ -7,11 +6,9 @@ function Pagenation() {
 
   return (
     <div className='bg-gray-400 text-center'>
-        {/* <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> */}
         <div><i class="fa-solid fa-arrow-left"></i></div>
         <h1>good</h1>
         <div><i class="fa-solid fa-arrow-right"></i></div>
-        {/* <FontAwesomeIcon icon="fa-solid fa-arrow-right" /> */}
     </div>
   )
 }
