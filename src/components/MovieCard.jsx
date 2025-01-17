6f4cb2e8e7392e9ad3eaefeddb87dfafc5c7c39d
Arun <arunm102301@gmail.com>
2025-01-17 20:07:18 +0530

    icon added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 // import Carddemo from "../../public/card1.webp";
 
-function MovieCard({poster_path, name}) {
+function MovieCard({ poster_path, name }) {
   return (
-    <div className="flex flex-col justify-center">
-      
+    <div className="flex flex-col items-center">
       <div
-        className="h-[50vh] sm:h-[300px] w-[50vw] sm:w-[200px] bg-cover flex items-end rounded-xl hover:cursor-pointer hover:scale-110 transition-transform duration-500 ease-in-out"
-        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})` }}
+        className="h-[50vh] sm:h-[300px] w-[50vw] sm:w-[200px] bg-cover flex flex-col justify-between rounded-xl hover:cursor-pointer hover:scale-110 transition-transform duration-500 ease-in-out"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})`,
+        }}
       >
-
-        <div className="">
-          &#12851;
+        <div className="m-4 self-end flex items-center justify-center h-8 w-8 bg-white bg-opacity-90 rounded-full shadow-lg">
+          {/* &#128525; */}
+          {/* <i class="fa-solid fa-heart" style={{color: `#ff0000`,}}></i> */}
+          <i class="fa-regular fa-heart" style={{color: `#ff0000`,}}></i>
         </div>
-       
-        <div className="flex w-full justify-center bg-black bg-opacity-50 flex-col rounded-b-xl">
+
+        <div className="flex w-full justify-center bg-black bg-opacity-50 rounded-b-xl">
           <h1 className="text-lg sm:text-xl font-bold text-white p-2 text-center">
-          {name}
+            {name}
           </h1>
         </div>
       </div>
-
-      
     </div>
   );
 }
