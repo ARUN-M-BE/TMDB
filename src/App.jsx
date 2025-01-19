258cd8f8fa6fe12c37ccd1edc09763f981497cc9
Arun <arunm102301@gmail.com>
2025-01-19 18:02:59 +0530

    sortIncreasing sortDecreasing

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ function App() {
             element={
               <Watchlist
                 watchlist={watchlist}
+                setWatchlist={setWatchlist}
               />
             }
           />
--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 // import moviecard from "../assets/card1.webp";
 
-function Watchlist({ watchlist }) {
-
-  const [search, setSearch] = React.useState('');
+function Watchlist({ watchlist, setWatchlist }) {
+  const [search, setSearch] = React.useState("");
 
   let handleSearch = (e) => {
-     setSearch(e.target.value);
+    setSearch(e.target.value);
+  };
+
+  let sortIncreasing = () => {
+    let sortedIncreas = watchlist.sort((movieA, movieB) => {
+      return movieA.vote_average - movieB.vote_average
+    })
+    setWatchlist([...sortedIncreas])
+  }
+
+  let sortDecreasing = () => {
+    let sortedDecreas = watchlist.sort((movieA, movieB) => {
+      return movieB.vote_average - movieA.vote_average
+    })
+    setWatchlist([...sortedDecreas])
   }
 
   const handleDelete = () => {
     console.log("delete");
-  }
+  };
 
   return (
     <>
@@ -26,8 +39,8 @@ function Watchlist({ watchlist }) {
 
       <div className="flex justify-center my-4">
         <input
-        onChange={handleSearch}
-        value={search}
+          onChange={handleSearch}
+          value={search}
           type="search"
           className="h-[3rem] w-[90%] md:w-[18rem] bg-gray-200 outline-none p-3 rounded-lg"
           placeholder="Search it"
@@ -40,47 +53,59 @@ function Watchlist({ watchlist }) {
             <thead className="border-b-2">
               <tr>
                 <th className="border-r-2 px-2 md:px-4">Movie Name</th>
-                <th className="border-r-2 px-2 md:px-4">Rating</th>
+                <th className="border-r-2 px-2 md:px-4 flex justify-center">
+                  <div onClick={sortIncreasing} className="p-2 hover:cursor-pointer duration-100 transition-transform"><i className="fa-solid fa-arrow-up"></i></div>
+                  <div className="p-2">Rating</div>
+                  <div onClick={sortDecreasing} className="p-2 hover:cursor-pointer duration-100 transition-transform"><i className="fa-solid fa-arrow-down"></i></div>
+                </th>
+
                 <th className="border-r-2 px-2 md:px-4"> Relased Year</th>
                 <th className="border-r-2 px-2 md:px-4">Genre</th>
                 <th className="px-2 md:px-4">Action</th>
               </tr>
             </thead>
             <tbody>
-              {watchlist.filter((movieObj) => {
-                return movieObj.title.toLowerCase().includes(search.toLocaleLowerCase());
-
-              }).map((movieObj) => {
-                  return  <tr className="border-b-2">
-                    <td className="border-r-2">
-                      <div className="flex flex-col md:flex-row items-center px-4 py-2">
-                        <img
-                          src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
-                          alt="moviecard"
-                          className="w-[150px] h-[80px] md:w-[200px] md:h-[100px] object-fit rounded"
-                        />
-                        <div className="ml-0 md:ml-4 mt-2 md:mt-0">
-                          {movieObj.title}
+              {watchlist
+                .filter((movieObj) => {
+                  return movieObj.title
+                    .toLowerCase()
+                    .includes(search.toLocaleLowerCase());
+                })
+                .map((movieObj) => {
+                  return (
+                    <tr className="border-b-2">
+                      <td className="border-r-2">
+                        <div className="flex flex-col md:flex-row items-center px-4 py-2">
+                          <img
+                            src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
+                            alt="moviecard"
+                            className="w-[150px] h-[80px] md:w-[200px] md:h-[100px] object-fit rounded"
+                          />
+                          <div className="ml-0 md:ml-4 mt-2 md:mt-0">
+                            {movieObj.title}
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="border-r-2 p-2 md:p-4">
-                      <div>{movieObj.vote_average}</div>
-                    </td>
-                    <td className="border-r-2 p-2 md:p-4">
-                      <div>{movieObj.release_date}</div>
-                    </td>
-                    <td className="border-r-2 p-2 md:p-4">
-                      <div>{movieObj.genre_ids.length}</div>
-                    </td>
-                    <td className="p-2 md:p-4">
-                      <button onClick={handleDelete} className="text-red-800 px-2 py-1 md:px-4 md:py-2 bg-gray-300 rounded-lg font-semibold hover:bg-red-800 hover:text-white">
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                
-              })}
+                      </td>
+                      <td className="border-r-2 p-2 md:p-4">
+                        <div>{movieObj.vote_average}</div>
+                      </td>
+                      <td className="border-r-2 p-2 md:p-4">
+                        <div>{movieObj.release_date}</div>
+                      </td>
+                      <td className="border-r-2 p-2 md:p-4">
+                        <div>{movieObj.genre_ids.length}</div>
+                      </td>
+                      <td className="p-2 md:p-4">
+                        <button
+                          onClick={handleDelete}
+                          className="text-red-800 px-2 py-1 md:px-4 md:py-2 bg-gray-300 rounded-lg font-semibold hover:bg-red-800 hover:text-white"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
             </tbody>
           </table>
         </div>
